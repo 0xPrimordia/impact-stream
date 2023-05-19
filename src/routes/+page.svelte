cec9cc7fc2202ffbf1cfe02b058a2dee44ae0b4a
Matthew Watman <mattwatman@gmail.com>
2023-05-19 12:07:20 -0400

    Initial pass at hero styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,68 @@
 <script lang="ts">
-    import Navbar from "../components/landing/Navbar.svelte";
-    import Valueprop from "../components/landing/Valueprop.svelte";
-    import Footer from "../components/landing/Footer.svelte";
-    export let data;
+	import Navbar from '../components/landing/Navbar.svelte';
+	import Valueprop from '../components/landing/Valueprop.svelte';
+	import Footer from '../components/landing/Footer.svelte';
+	export let data;
 </script>
-<style>
-    .wrapper section {
-        max-width: 960px;
-        margin: auto;
-    }
-</style>
 
 <Navbar />
 <div class="wrapper">
-    <section>
-        <h1>{data.hero.title}</h1>
-        <p>{data.hero.sub}</p>
-    </section>
+	<section>
+		<h1>{data.hero.title}</h1>
+		<p>{data.hero.sub}</p>
+		<button>Donate</button>
+	</section>
 </div>
 <Valueprop />
 <div class="wrapper">
-    <section>
-        <h1>Explainer Section</h1>
-        <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-    </section>
+	<section>
+		<h1>Explainer Section</h1>
+		<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	</section>
 </div>
 <div class="wrapper">
-    <section>
-        <h1>Team Section</h1>
-        <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-    </section>
+	<section>
+		<h1>Team Section</h1>
+		<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	</section>
 </div>
 <div class="wrapper">
-    <section>
-        <h1>Partners</h1>
-        <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-    </section>
+	<section>
+		<h1>Partners</h1>
+		<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	</section>
 </div>
 <Footer />
+
+<style>
+	.wrapper section {
+		max-width: 960px;
+		margin: auto;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.wrapper section h1 {
+		font-size: 4rem;
+		color: #3e5f72;
+		margin: 7.5rem 0 0 0;
+	}
+
+	.wrapper section p {
+		font-size: 2.5rem;
+		margin-bottom: 3rem;
+	}
+
+	.wrapper section button {
+		background-color: #000;
+		color: #ebf4e6;
+		border: none;
+		padding: 1rem 2rem;
+		border-radius: 10px;
+		font-size: 1.5rem;
+		font-weight: bold;
+		width: 33.3%;
+	}
+</style>
+
