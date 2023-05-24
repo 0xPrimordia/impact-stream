d64ef4d63d699f93bb8d9489a2a169ef3a06436d
Matthew Watman <mattwatman@gmail.com>
2023-05-24 12:10:18 -0400

    Update button and icon styles

--- src/components/landing/Footer.svelte
+++ src/components/landing/Footer.svelte
@@ -61,78 +61,110 @@
 	div.ripple-border {
 		height: 40px;
 		background: top center transparent url('/images/ripple-border.svg');
-		background-size: cover; 
-    }
-    div.ripple-line {
-        background: top center transparent url('/images/ripple-line.svg') no-repeat;
-        background-size: 100%;
-        height: 40px;
-    }
-    div.ripple-line-blue {
-        background: top center transparent url('/images/ripple-line-blue.svg') no-repeat;
-        background-size: 100%;
-        height: 40px;
-    }
-    footer.footer .bottom {
-        display: flex;
-        align-items: center;
-        padding-left: 2em;
-    }
-    footer.footer .bottom img {
-        width: 2em;
-        height: 2em;
-    }
-    div.footer-link {
-        text-align: right;
-        line-height: 2;
-        padding: 2em;
-        font-size: 0.9em;
-        margin-left: auto;
-    }
-    div.footer-link a {
-        color: white;
-        text-decoration: none;
-    }
-    div.footer-link a:hover {
-        text-decoration: underline;
-    }
-    .footer-cta {
-        display: flex;
-        padding: 2em;
-        max-width: 960px;
-        margin: auto;
-        align-items: center;
-    }
-    .footer-cta .copy {
-        font-size: 2em;
-        font-weight: bold;
-        color: #EBE8CC;
-    }
-    .donate {
-        max-width: 960px;
-        margin: auto;
-        padding: 4em 0;
-    }
-    .donate .copy {
-        font-size: 2em;
-        font-weight: bold;
-        color: #3E5F72;
-    }
-    .footer-cta button {
-        background: #EBE8CB;
-        border-radius: 5px;
-        color: rgba(105,132,146,1);
-        font-size: 1em;
-        border: none;
-        font-weight: bold;
-        padding: 1em 3em;
-    }
-    .footer-cta .button-wrap {
-        margin-left: auto;
-    }
-    @media (max-width: 950px) {
-        .donate {
-            padding: 4rem 2rem;
-        }
-    }
+		background-size: cover;
+	}
+	div.ripple-line {
+		background: top center transparent url('/images/ripple-line.svg') no-repeat;
+		background-size: 100%;
+		height: 40px;
+	}
+	div.ripple-line-blue {
+		background: top center transparent url('/images/ripple-line-blue.svg') no-repeat;
+		background-size: 100%;
+		height: 40px;
+	}
+	footer.footer .bottom {
+		display: flex;
+		align-items: center;
+		padding-left: 2em;
+	}
+	footer.footer .bottom img {
+		width: 2em;
+		height: 2em;
+	}
+	div.footer-link {
+		text-align: right;
+		line-height: 2;
+		padding: 2em;
+		font-size: 0.9em;
+		margin-left: auto;
+	}
+	div.footer-link a {
+		color: white;
+		text-decoration: none;
+	}
+	div.footer-link a:hover {
+		text-decoration: underline;
+	}
+	.footer-cta {
+		display: flex;
+		padding: 2em;
+		max-width: 960px;
+		margin: auto;
+		align-items: center;
+	}
+	.footer-cta .copy {
+		font-size: 2em;
+		font-weight: bold;
+		color: #ebe8cc;
+	}
+	.donate {
+		display: flex;
+		width: 960px;
+		margin: auto;
+		padding: 4em 0;
+	}
+	.donate .copy {
+		font-size: 2em;
+		font-weight: bold;
+		color: #3e5f72;
+	}
+	.donate .buttons {
+		display: flex;
+		flex-direction: column;
+		margin-left: auto;
+	}
+	.buttons button {
+		background: #3d5e71;
+		border-radius: 15px;
+		color: rgba(234, 232, 206, 1);
+		font-size: 1em;
+		border: none;
+		font-weight: bold;
+		padding: 1em 3em;
+	}
+	.buttons button .copy-icon {
+		width: 1.5em;
+		height: 1.5em;
+		position: relative;
+		top: 0.2em;
+		left: 0.2em;
+	}
+
+	.buttons button .check-icon {
+		width: 1.5em;
+		height: 1.5em;
+		position: relative;
+		top: 0.2em;
+		left: 0.2em;
+	}
+	.buttons button:first-of-type {
+		margin-bottom: 1.5em;
+	}
+
+	.buttons button:hover {
+		color: rgba(105, 132, 146, 0.5);
+	}
+	.footer-cta button {
+		background: #ebe8cb;
+		border-radius: 5px;
+		color: rgba(105, 132, 146, 1);
+		font-size: 1em;
+		border: none;
+		font-weight: bold;
+		padding: 1em 3em;
+	}
+	.footer-cta .button-wrap {
+		margin-left: auto;
+	}
 </style>
