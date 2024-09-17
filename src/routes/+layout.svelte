68938d00545bb0d4b8d6a82e8229c7bdede63fb0
Will Little <wclittle@gmail.com>
2024-09-17 15:46:03 -0700

    adding files for our new header tooling

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { setContext } from 'svelte';
+  import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+  {#if $page.url.pathname === '/'}
+    <title>Impact Stream | Digital → IRL Impact</title>
+    <meta name="description" content="We are decentralizing poverty alleviation and creating transparent streams of impact to developing communities." />
+    <meta property="og:title" content="Impact Stream | Digital → IRL Impact" />
+    <meta property="og:description" content="We are decentralizing poverty alleviation and creating transparent streams of impact to developing communities." />
+    <meta property="og:image" content="https://impact.stream/images/og-image-2.png" />
+  {/if}
+</svelte:head>
+
+<slot />
---
+++ src/routes/quirkies/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { setContext } from 'svelte';
+  import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+  {#if $page.url.pathname.startsWith('/quirkies')}
+    <title>Togo Water Campaign with Quirkies</title>
+    <meta name="description" content="Impact Stream is partnering with Quirkies to build wells and bring clean water to both a village and an orphanage in Togo, West Africa." />
+    <meta property="og:title" content="Togo Water Campaign with Quirkies" />
+    <meta property="og:description" content="Impact Stream is partnering with Quirkies to build wells and bring clean water to both a village and an orphanage in Togo, West Africa." />
+    <meta property="og:image" content="https://impact.stream/images/quirkies-og-image.png" />
+  {/if}
+</svelte:head>
+
+<slot />
